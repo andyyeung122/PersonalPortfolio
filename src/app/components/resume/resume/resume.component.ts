c72031dfeb41c12f8ae33e01dc709af97a992e11
Andy Yeung <andyyeung122@gmail.com>
2020-08-15 18:50:47 -0400

    added navigation for resume page

--- src/app/components/resume/resume/resume.component.ts
+++ src/app/components/resume/resume/resume.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-resume',
@@ -8,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 export class ResumeComponent implements OnInit {
   pdfResume = "../../../../assets/pdf/AndyYeungResume.pdf";
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  public goToHome() {
+    this.router.navigateByUrl('/home');
   }
 
 }
