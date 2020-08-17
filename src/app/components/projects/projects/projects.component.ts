acd333bd2219d315e53bba0899df36f89831840b
Andy Yeung <andyyeung122@gmail.com>
2020-08-17 19:05:24 -0400

    set model for each Project

--- src/app/components/projects/projects/projects.component.ts
+++ src/app/components/projects/projects/projects.component.ts
@@ -1,5 +1,7 @@
+import { Project } from './../../../models/project';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-projects',
