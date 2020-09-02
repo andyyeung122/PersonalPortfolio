f07b82e2cbfd4c04336f2e597b9c0761278b949f
Andy Yeung <andyyeung122@gmail.com>
2020-09-02 18:03:39 -0400

    added contact dialog

---
+++ src/app/dialogs/contact-dialog/contact-dialog.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialogModule, MatDialogRef, MatDialogConfig } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-contact-dialog',
+  templateUrl: './contact-dialog.component.html',
+  styleUrls: ['./contact-dialog.component.scss']
+})
+export class ContactDialogComponent implements OnInit {
+
+  sendEmailForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private dialogRef: MatDialogRef<ContactDialogComponent>) {
+    this.buildForm();
+   }
+
+  ngOnInit(): void {
+  }
+
+  public buildForm() {
+    this.sendEmailForm = this.formBuilder.group({
+      name: ['', Validators.compose([Validators.required, Validators.minLength(2)])],
+      email: ['', Validators.compose([Validators.required, Validators.email, Validators.minLength(2)])],
+      company: ['', Validators.compose([Validators.required, Validators.minLength(2)])],
+      message: ['', Validators.compose([Validators.required, Validators.minLength(2)])]
+    });
+  }
+
+  public onClose() {
+    this.dialogRef.close();
+  }
+
+  public onSubmit() {
+
+    this.dialogRef.close(this.sendEmailForm);
+  }
+
+}
